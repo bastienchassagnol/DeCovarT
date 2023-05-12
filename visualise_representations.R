4a545695d57c3562ee44df10e028ae6bf58f3076
bastienchassagnol <bastien_chassagnol@laposte.net>
2023-05-12 19:03:52 +0200

    add figs and datasets v2

--- visualise_representations.R
+++ visualise_representations.R
@@ -6,53 +6,35 @@
 ###########################################################################
 ###########################################################################
 
-#################################################################
-##        compare own deconvolution in complex scenario        ##
-#################################################################
-complex_simulation <- readRDS("./simulations/results/complex_scenario.rds")
-
-complex_simulation_heatmap <- list()
-for (scen_prop in unique(complex_simulation$proportions)) {
-  for (scen_var in unique(complex_simulation$variance)) {
-    name_scenario <- paste0(scen_var, " with ", scen_prop, " ratios") %>% stringr::str_to_title()
-    heatmap_list <- complex_simulation %>% 
-      dplyr::filter(proportions==scen_prop & variance==scen_var) %>% 
-      plot_correlation_Heatmap()
-    heatmap_page <- purrr::imap(heatmap_list,   ~ ComplexHeatmap::draw(.x, padding=unit(c(0, 0, 0, 0), "cm"),
-                                                                       column_title= .y, column_title_gp = grid::gpar(fontsize = 12, fontface="bold")) %>% 
-                                  grid::grid.grabExpr())
-    complex_simulation_heatmap[[name_scenario]] <- gridExtra::arrangeGrob(grobs = heatmap_page, ncol = 2,  padding = unit(0.1, "line"),
-                                                                       top=ggpubr::text_grob(name_scenario, size = 18, face = "bold"))
-  }
-}
-
-ggsave("./figs/Heatmap_complex_example.pdf", 
-       gridExtra::marrangeGrob(grobs=complex_simulation_heatmap, top="", ncol = 1, nrow = 1), width = 12, height = 20,dpi = 300)
-
 ##################################################################
 ##      generate WABi general complexHeatmap       ##
 ##################################################################
 
-
+library(dplyr)
 bivariate_simulation <- readRDS("./simulations/results/bivariate_scenario.rds")
-
 bivariate_simulation_test <- bivariate_simulation %>%
-  mutate(ID = case_when(
-    proportions == "balanced" & centroids=="small CLD" ~ "B1_",
-    proportions == "highly_unbalanced" & centroids=="small CLD" ~ "B2_",
-    proportions == "balanced" & centroids=="high CLD" ~ "B3_",
-    proportions == "highly_unbalanced" & centroids=="high CLD" ~ "B4_",
-    TRUE                      ~ as.character(ID) )) %>% 
-  dplyr::mutate(ID = dplyr::if_else(variance=="homoscedasctic", paste0(ID, "Ho"), paste0(ID, "He")))
-saveRDS(bivariate_simulation_test, "./simulations/results/bivariate_scenario.rds")
+  mutate(algorithm=factor(algorithm, levels = c("lm", "nnls", "lsei",
+                                                "gradient", "hessian", "DeCoVarT",
+                                                "optim", "barrier", "SA"))) %>%
+  mutate(algorithm=forcats::fct_recode(algorithm, Levenberg = "DeCoVarT") %>% 
+           forcats::fct_relevel())
+saveRDS(bivariate_simulation_test %>% dplyr::select(-c("proportions", "true_parameters",
+                                                       "variance", "overlap", "model_coef_determination", 
+                                                       "model_coef_determination_adjusted", "model_cor",
+                                                       "entropy", "centroids")) %>% 
+          dplyr::relocate("ID", .before = "correlation_celltype1") %>% 
+          dplyr::rename_with(~gsub("celltype_", "p", .x)), 
+        file = "./data/bivariate/bivariate_parameters.rds")
 
-bivariate_simulation_heatmap <- list()
-
-
-
-
-# for (scen_prop in unique(highly_overlapping_simulation$proportions)) {
-#   for (scen_var in unique(highly_overlapping_simulation$variance)) {
+# bivariate_simulation <- bivariate_simulation %>%
+#   mutate(ID = case_when(
+#     proportions == "balanced" & centroids=="small CLD" ~ "B1_",
+#     proportions == "highly_unbalanced" & centroids=="small CLD" ~ "B2_",
+#     proportions == "balanced" & centroids=="high CLD" ~ "B3_",
+#     proportions == "highly_unbalanced" & centroids=="high CLD" ~ "B4_",
+#     TRUE                      ~ as.character(ID) )) %>% 
+#   dplyr::mutate(ID = dplyr::if_else(variance=="homoscedasctic", paste0(ID, "Ho"), paste0(ID, "He")))
+# saveRDS(bivariate_simulation, "./simulations/results/bivariate_scenario.rds")
 
 splitted_heatmap <- split(x = bivariate_simulation_test, f = bivariate_simulation_test$ID)
 bivariate_simulation_heatmap <- purrr::imap(splitted_heatmap, function (.data, .name_scenario) {
@@ -66,9 +48,18 @@ bivariate_simulation_heatmap <- purrr::imap(splitted_heatmap, function (.data, .
   return(heatmap_page)
 })
 
+ggsave("./figs/bivariate_Heatmaps_test.pdf", 
+       gridExtra::marrangeGrob(grobs=bivariate_simulation_heatmap, top="", ncol = 1, nrow = 1),
+       width = 12, height = 12,dpi = 300)
 
-ggsave("./figs/bivariate_Heatmaps.pdf", 
-       gridExtra::marrangeGrob(grobs=bivariate_simulation_heatmap, top="", ncol = 1, nrow = 1), width = 12, height = 12,dpi = 300)
+library(dplyr)
+bivariate_formatted <- readRDS("./data/bivariate/bivariate_parameters.rds") %>% 
+  
+saveRDS(bivariate_formatted %>% dplyr::mutate(across(where(is.numeric), signif, 4)) %>% 
+          dplyr::select(-c("model_rmse", "model_mae")),
+        "./data/bivariate/bivariate_parameters.rds")
+
+
 
 
 #################################################################
